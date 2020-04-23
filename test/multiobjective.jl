941f2215c336eeccb96cf410e74ef287aef0dc03
Eric S Fraga <e.fraga@ucl.ac.uk>
2020-04-23 22:39:29 +0100

    test files now in the test directory

---
+++ test/multiobjective.jl
@@ -0,0 +1,28 @@
+# [[file:~/s/research/julia/Fresa.jl/fresa.org::testmultiobjective][testmultiobjective]]
+using Fresa
+nx = 2
+a = zeros(nx)
+b = ones(nx)
+x = rand(nx)
+f = x -> ( [sin(x[1]-x[2]); cos(x[1]+x[2])], 0)
+pareto, population = Fresa.solve(f, x, a, b;
+                                 #fitnesstype = :hadamard,
+                                 #fitnesstype = :borda,
+                                 fitnesstype = :nondominated,
+                                 ngen=100,
+                                 npop=10,
+                                 plotvectors=true,
+                                 tolerance=0.01)
+
+println("Pareto front:")
+println(population[pareto])
+#using BenchmarkTools
+#@benchmark
+
+using PyPlot
+z = [population[pareto[i]].z for i in 1:length(pareto)];
+PyPlot.plot([z[i][1] for i=1:length(z)],
+            [z[i][2] for i=1:length(z)],
+            "ro")
+PyPlot.savefig("x.pdf")
+# testmultiobjective ends here
